a0e92df15d7601a9e7ee7bddcc000a6332d46fe8
Willkins kenyatta <128659684+Willkins3951@users.noreply.github.com>
2024-03-07 02:31:56 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+
+function mySignUpFunc(){
+    document.getElementById('login-form').style.display='none';
+    document.getElementById('sign-up-form').style.display='block';
+}        
+function myLoginFunc(){
+    document.getElementById('login-form').style.display='block';
+    document.getElementById('sign-up-form').style.display='none';
+}
